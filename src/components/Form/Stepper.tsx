55cd7c97611ac9afd690d7b7fda7f5f554ffd20e
GabrielP <gugapoeta@gmail.com>
2024-10-08 09:30:22 -0400

    fix(multipleSteapForm): adjust color of buttons and steps

--- src/components/Form/Stepper.tsx
+++ src/components/Form/Stepper.tsx
@@ -5,6 +5,32 @@ import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Button from "@mui/material/Button";
 import StepButton from "@mui/material/StepButton";
+import { createTheme, ThemeProvider} from "@mui/material";
+import Typography from '@mui/material/Typography';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: '#ba8638',
+    },
+    text: {
+      primary: '#3f3f46',
+      secondary: '#46505A',
+    },
+    action: {
+      active: '#ba8638',
+    },
+    success: {
+      main: '#009688',
+    }
+  },
+  typography: {
+    button: {
+      "fontWeight": 500,
+       "textTransform": 'capitalize',
+    },
+  },
+});
 
 type Step = {
   label: string;
@@ -29,41 +55,107 @@ export function Steps({ items }: StepProps) {
   const isLastStep = activeStep === items.length - 1;
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <ThemeProvider theme={theme}>
+      <Typography>
+          <Box
+        sx={{
+          // bgcolor: 'background.paper',
+          // boxShadow: 1,
+          // borderRadius: 2,
+          // p: 2,
+          // minWidth: 300,
+        }}
+      >
+
       <Stepper activeStep={activeStep} nonLinear>
         {items.map(({ label, hasError }, index) => {
           return (
-            <Step key={label}>
+            <Step 
+            key={label}
+            sx={{
+              '.css-15rfbsb-MuiSvgIcon-root-MuiStepIcon-root.Mui-active':{
+                color:'#ba8638'
+              }
+
+            }}
+            >
               <StepButton onClick={() => setActiveStep(index)}>
-                <StepLabel error={hasError}>{label}</StepLabel>
+                <StepLabel  error={hasError}>{label}</StepLabel>
               </StepButton>
             </Step>
           );
         })}
       </Stepper>
 
-      <Box minHeight={"30vh"}>{items[activeStep].Component}</Box>
+      <Box sx={{
+          // boxShadow: 1,
+          // borderRadius: 2,
+          // p: 2,
+          // minWidth: 300,
+      }}
+      minHeight={"30vh"}>{items[activeStep].Component}</Box>
 
-      <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+      <Box sx={{ 
+        display: "flex", 
+        flexDirection: "row", 
+        pt: 2, 
+        
+        }}
+        >
+        
+        {/* // Active button at the bottom */}
         <Button
           color="inherit"
           disabled={activeStep === 0}
           onClick={handleBack}
-          sx={{ mr: 1 }}
-        >
+          sx={{ 
+            mr: 1, 
+            bgcolor: activeStep === 0 ? 'transparent' : '#d6b56e',
+            color: '#f9f6ed',
+            fontSize: 16,
+            p: 2,
+            borderRadius: 3,
+            // boxShadow: 1,
+          }}
+          className="rounded h-10 bg-white hover:bg-goldCordas_200 hover:text-goldCordas_50">
           Voltar
         </Button>
-        <Box sx={{ flex: "1 1 auto" }} />
+
+        <Box
+        sx=
+        {{ 
+          flex: "1 1 auto",
+          
+          }} 
+          />
+          
         {isLastStep ? (
           <Button key="enviar" type="submit">
             Enviar
           </Button>
         ) : (
-          <Button key="proximo" type="button" onClick={handleNext} className="bg-goldCordas_300 rounded font-semibold text-white h-10 hover:bg-goldCordas_200">
+
+          <Button 
+          key="proximo" 
+          type="button" 
+          onClick={handleNext} 
+          sx={{ 
+            mr: 1, 
+            color: '#f9f6ed',
+            fontSize: 16,
+            p: 2,
+            backgroundColor: '#d6b56e',
+            borderRadius: 3,
+            // boxShadow: 1,
+          }}
+          className="rounded h-10 hover:bg-goldCordas_200 hover:text-goldCordas_50">
             Próximo
           </Button>
         )}
       </Box>
+
     </Box>
+    </Typography>
+    </ThemeProvider>
   );
 }
